9a614f7fcb66607f4caed2487be1897832aea8ef
Rafal <bokstur.lodz@gmail.com>
2019-04-16 18:35:35 +0200

    update

---
+++ Bike.py
@@ -0,0 +1,11 @@
+
+
+class Bike:
+
+    def __init__(self, Id, Color, Size, Brand):
+        self.Id = Id
+        self.Color = Color
+        self.Size = Size
+        self.Brand = Brand
+
+        
